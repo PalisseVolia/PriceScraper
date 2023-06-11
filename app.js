0126df71a7bb84bdf61c8b84a167b9549cb48c91
PalisseVolia <palissev@gmail.com>
2023-06-11 23:04:08 +0200

    "working"

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-    const data = await webScraper("https://www.amazon.fr/SABRENT-Interne-Performance-extr%C3%AAme-SB-RKT4P-8TB/dp/B09WZK8YMY/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_w=8CwaL&content-id=amzn1.sym.5ff22ff7-b8e2-443a-8dfb-67b22e8a7de3&pf_rd_p=5ff22ff7-b8e2-443a-8dfb-67b22e8a7de3&pf_rd_r=84JHMTC3PH07TA4MVWPP&pd_rd_wg=q8eUl&pd_rd_r=2072cddf-32a7-4fd1-bdb9-ea28c4319af7&pd_rd_i=B09WZK8YMY");
-    res.send(data);
+    const data = await webScraper("https://www.amazon.fr/Pilot-Hi-Tecpoint-stylos-bille-Pointe/dp/B00LXAOICW?pd_rd_w=PzeqQ&content-id=amzn1.sym.8f917f30-0e7e-4c71-8b5f-1e2cdeb046b8&pf_rd_p=8f917f30-0e7e-4c71-8b5f-1e2cdeb046b8&pf_rd_r=VZW1K9CVH38CPMEQA85M&pd_rd_wg=rljYz&pd_rd_r=0dd1c8be-5ddb-435f-af6e-e03bd39afa35&pd_rd_i=B00LXAOICW&psc=1&ref_=pd_bap_d_grid_rp_0_1_ec_i");
+    const { date, product, price } = data;
+
+    const html = await ejs.renderFile(path.join(__dirname, "index.ejs"), { date, product, price });
+
+    res.send(html);
 });
+
+app.use(express.static(path.join(__dirname, "/")));
 
 async function webScraper(url) {
     const browser = await puppeteer.launch({ headless: "new" });
